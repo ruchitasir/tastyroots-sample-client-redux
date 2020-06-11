5867a614c492cd13ccf1256a437efe6e5c5f32e7
essleeung <essleeung@gmail.com>
2020-06-10 22:33:21 -0400

    updated profile page setup

--- src/content/pages/Home.js
+++ src/content/pages/Home.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import Signup from './Signup'
+import { Container } from 'semantic-ui-react'
 
 const Home = props => {
   if (props.user) {
     return (
-      <div>
+      <Container className="top-spacing">
         <h1>Welcome to Tasty Roots!</h1>  
-      </div>
+      </Container>
     )
   }
   return (
-    <div>
+    <Container className="top-spacing">
       <Signup user={props.user} updateToken={props.updateToken}/>    
-    </div>
+    </Container>
   )
 }
 
